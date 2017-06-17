43f654c68d4da54b7417ebfe1109df1823b3b83b
codervince <publicvince102@gmail.com>
2017-06-17 02:27:26 +0200

    real data from mongodb

--- data/schema.js
+++ data/schema.js
@@ -9,10 +9,99 @@ import {
   GraphQLSchema,
   GraphQLString,
 } from 'graphql'; //from reference implementation
-
 import moment from 'moment-timezone';
-
 import { GraphQLDate} from 'graphql-scalars';
+
+
+
+
+//JS trick
+let schemaFunc = (db) => {
+
+      let counter = 1; //init value
+
+      function getDecimalPrice(numerator, denominator){
+        return (numerator+denominator)/denominator;
+      }
+
+      //func reference
+      let fractional2Decimal = function(p){
+        p = ((p/10)+1).toString() + '/1'
+        return p;
+      }
+
+      let runnerType = new GraphQLObjectType({
+        name: 'Runner',
+        description: 'Horse data for race',
+        fields: {
+          horse: {type: GraphQLString},
+          price: {type: GraphQLInt},
+          decimalPrice: {type: GraphQLInt,
+            resolve: (horse) => horse.price
+          },
+          englishPrice: { type: GraphQLString,
+          resolve: (horse)=> fractional2Decimal(horse.price) }
+        }
+      });
+
+      let raceType =new GraphQLObjectType({
+        name: 'Race',
+        description: 'race-specific information',
+        fields: {
+          racenumber: {type: GraphQLInt},
+          runners: {type: new GraphQLList(runnerType)}
+        }
+      });
+
+      let meetingType =new GraphQLObjectType({
+        name: 'Meeting',
+        description: 'Meet information',
+        fields: {
+            racedate: {type: GraphQLString},
+            racecourse: {type: GraphQLString},
+            noraces: {type: GraphQLInt},
+            races: {type: new GraphQLList(raceType)}
+            }
+        });
+
+
+        var schema = new GraphQLSchema({
+            query: new GraphQLObjectType({
+              name: 'RootQueryType', //required
+
+              fields: { //required
+
+                hkjcresults: {
+                  type: new GraphQLList(meetingType),
+                  description: "Takes 2 arguments: racedate & racecourse - must be unique - returns all the meeting, race, runner data for that event",
+                  args: {
+                    racedate : {type:GraphQLString },
+                    racecourse : {type:GraphQLString }
+                  },
+                  resolve(_, args) {
+                    // presently we only have 1 event stored later remove this condition
+                    if (args.racedate == '20170614' && args.racecourse =='HV') {
+                      return db.collection("HKJCruns").find({}).toArray(); //returns Promise taken care of automagically
+                    }else {
+                      return [`nothing here yet for ${args.racedate} - ${args.racecourse} - ${args.racenumber}`]; //dummy string
+                      }
+                    }
+                  }, //end hkjcresult
+                random: {
+                  type: GraphQLFloat,
+                  resolve() {
+                    return Math.random();
+                  }
+                }
+              }//end fields
+          })
+        });
+
+      return schema;
+}
+
+
+
 
 /*
 resolve() {
@@ -23,87 +112,8 @@ resolve: () => 'world'
 
 */
 
-function getDecimalPrice(numerator, denominator){
-  return (numerator+denominator)/denominator;
-}
 
-//func reference
-let fractional2Decimal = function(p){
-  p = ((p/10)+1).toString() + '/1'
-  return p;
-}
 
-// let top4_20170614HV1 = ['T333', 'S245', 'T056', 'V119'];
-let runner1 = {
-      horse:'T333',
-      price: 33
-    }
-let top4_20170614HV1 = [
-  {horse:'T333',price: 33},
-  {horse:'S245',price: 79},
-  {horse:'T056',price: 230},
-  {horse:'V119',price: 190}
-];
-    // },
-    // second: {
-    //   horse:'S245',
-    //   price: 79
-    // },
-    // third: {
-    //   horse:'T056',
-    //   price: 230
-    // },
-    // fourth: {
-    //   horse:'V119',
-    //   price: 190
-    // }
 
-let runnerType =new GraphQLObjectType({
-  name: 'top4_20170614HV1',
-  fields: {
-    horse: {type: GraphQLString},
-    price: {type: GraphQLInt,
-          deprecationReason: "We dont need this"
-    },
-    decimalPrice: {type: GraphQLInt,
-      resolve: (horse) => horse.price
-    },
-    englishPrice: { type: GraphQLString,
-    resolve: (horse)=> fractional2Decimal(horse.price) }
-    }
-  });
 
-let counter = 1; //init value
-
-var schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-      name: 'RootQueryType', //required
-      fields: { //required
-        hkjcresult: {
-          type: new GraphQLList(runnerType),
-          description: "Needs 3 arguments: racedate-racecourse-racenumber - returns first 4 horsecodes in order plus URL",
-          args: {
-            racedate : {type:GraphQLString },
-            racecourse : {type:GraphQLString },
-            racenumber : {type:GraphQLInt }
-          },
-          resolve(_, args) {
-            // return top4_20170614HV1;
-            if (args.racedate == '20170614' && args.racecourse =='HV' && args.racenumber == 1) {
-              return top4_20170614HV1;
-            }else {
-              return [`nothing here yet for ${args.racedate} - ${args.racecourse} - ${args.racenumber}`];
-              }
-            }
-          },
-        random: {
-          type: GraphQLFloat,
-          resolve() {
-            return Math.random();
-          }
-        }
-      }//end fields
-  })
-});
-
-export default schema;
+export default schemaFunc;
--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 // const graphQLHTTP = require('express-graphql');
 import graphQLHTTP from 'express-graphql';
 // var Schema = require('./data/schema');
-import Schema from "./data/schema";
-
+import schemaFunc from "./data/schema";
+const express = require('express')
 
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
 
   // Use connect method to connect to the server
-  MongoClient.connect(process.env.MONGO_URI, function(err, db) {
-    console.log(process.env.MONGO_URI);
+MongoClient.connect(process.env.MONGO_URI, function(err, db) {
+
     assert.equal(null, err);
+
     console.log("Connected successfully to server");
 
-    var cursor = db.collection('races').find();
-    cursor.each(function(err,doc){
-      assert.equal(err, null);
-      if (doc!= null){
-        console.dir(doc)
-      }
-    })
+    // var cursor = db.collection('races').find();
+    // cursor.each(function(err,doc){
+    //   assert.equal(err, null);
+    //   if (doc!= null){
+    //     console.dir(doc)
+    //   }
+    // })
 
     // db.close();
+    const app = express();
+    app.use('/graphql', graphQLHTTP({schema: schemaFunc(db), graphiql: true }))
+
+
+    app.get('/', function (req, res) {
+      res.send('Hello World!')
+    })
+
+    app.listen(3010, function () {
+      console.log('Example app listening on port 3010!')
+    })
+
   });
-  const express = require('express')
-  const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-//from express-graphql repo MOUNT onto /graphql
-app.use('/graphql', graphQLHTTP({schema: Schema, graphiql: true }))
-
-
-app.listen(3010, function () {
-  console.log('Example app listening on port 3010!')
-})
